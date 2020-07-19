44d316754f10d56938825943183c4358a30a2449
Weidong Meng <>
2020-07-19 21:58:50 +0800

    修改文件

---
+++ MMSDK/MMHead.h
@@ -0,0 +1,19 @@
+//
+//  MMHead.h
+//  spetest
+//
+//  Created by Weidong Meng on 2020/7/19.
+//  Copyright © 2020 mwd. All rights reserved.
+//
+
+#ifndef MMHead_h
+#define MMHead_h
+
+#import "UIView+YXAddition.h"
+#import "UIColor+YXAddition.h"
+#import "UIImage+YXAddition.h"
+#import "UILabel+YXAction.h"
+#import "UIView+Badge.h"
+
+
+#endif /* MMHead_h */
--- MMSDKExample/MMSDKExample/ViewController.m
+++ MMSDKExample/MMSDKExample/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "MMHead.h"
 @interface ViewController ()
 
 @end
